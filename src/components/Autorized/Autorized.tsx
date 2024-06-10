97111961e4755eb700caa7256e1da65f817ab347
unknown <UstinovichSergey>
2024-06-10 08:14:31 +0300

    icon fix

--- src/components/Autorized/Autorized.tsx
+++ src/components/Autorized/Autorized.tsx
@@ -1,20 +1,24 @@
 import { Button } from "../../utils/Button"
 import AvatarNoName from "../../assets/svg/AvatarNoName/AvatarNoName"
 import style from './Autorized.module.scss'
-import { useState } from "react"
+import { useCallback, useState } from "react"
 import Modal from "../../utils/Modal/Modal"
 
 function Autorized() {
 
-    const [isOpen, setIsOpen] = useState(false)
+    const [isOpenModal, setIsOpenModal] = useState(false)
+
+    const onToggleModal = useCallback(()=> {
+        setIsOpenModal((prev) => !prev)
+    }, [isOpenModal])
 
     return(
         <div>
-            <Button className={style.avatar} onClick={()=> setIsOpen(true)}>
+            <Button className={style.avatar} onClick={onToggleModal}>
                 <AvatarNoName className={style.icon} />
                 <p>Войти</p>
             </Button>
-            <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+            <Modal isOpen={isOpenModal} onClose={onToggleModal}>
                 Привет, тут скоро будет Вход/Регистрация, а сейчас до свидания!
             </Modal>
         </div>
