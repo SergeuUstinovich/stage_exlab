7633ddaa24f44c537de186e76d7129a5cc000cf4
bigmazykz <bigmazy604@gmail.com>
2024-06-17 19:27:02 +0300

    Login_form

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,17 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
+import style from './Layout.module.scss'
 
 function Layout() {
     return (
         <>
             <Navbar />
-            <main>
-                <div className='container'>
+            <main className={style.page__content}>
+                <div className={style.container}>
                     <Outlet />
                 </div>
             </main>
             <footer>
-                <div className='container'>
+                <div className={style.container}>
 
                 </div>
             </footer>
