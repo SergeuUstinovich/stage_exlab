3414e11b4b313c5b56bd30ccff8cd7ae4a77d2cd
unknown <UstinovichSergey>
2024-07-01 19:51:03 +0300

    Updates

--- assets/MainPage-39a8a45d.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-474f4a91.js";function n(){return a.jsx("div",{className:"header__container"})}export{n as default};
---
+++ assets/MainPage-be3e1a77.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-6ca79ebf.js";function n(){return a.jsx("div",{className:"header__container"})}export{n as default};
