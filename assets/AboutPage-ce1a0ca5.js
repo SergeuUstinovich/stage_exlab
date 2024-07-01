b5cd5c7dcd9de385f576499355fb2ba7f923179e
unknown <UstinovichSergey>
2024-07-01 20:38:27 +0300

    Updates

--- assets/AboutPage-ce1a0ca5.js
+++
@@ -1,1 +0,0 @@
-import{j as t}from"./index-444313cf.js";function a(){return t.jsx("div",{className:"about",children:"Тут тоже идут работы"})}export{a as default};
---
+++ assets/MainPage-9d78622b.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-2eab6e90.js";function n(){return a.jsx("div",{className:"header__container"})}export{n as default};
