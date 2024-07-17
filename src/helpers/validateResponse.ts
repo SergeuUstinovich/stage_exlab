486267564f44bd58c215a3950a15581e4893a5b3
unknown <UstinovichSergey>
2024-07-17 20:50:30 +0300

    fixFetch

--- src/helpers/validateResponse.ts
+++ src/helpers/validateResponse.ts
@@ -1,15 +1,17 @@
-export function validateError(error: any): Promise<never> {
-    if(error.response) {
-        const nonFieldErrors = error.response.data.errors.non_field_errors || ['Возникла ошибка'];
-        throw new Error(nonFieldErrors.join(' '));
-    }
-    throw new Error('Возникла неизвестная ошибка');
-}
-
-export function validateErrorForgotEmail(error: any): Promise<never> {
-    if(error.response) {
-        const nonFieldErrors = error.response.data.errors || 'Возможны проблемы с сетью';
+export async function validateError(response: Response): Promise<Response> {
+    if(!response.ok) {
+        const errorObject = await response.json();
+        const nonFieldErrors = errorObject.errors.non_field_errors || 'Возможны проблемы с сетью';
         throw new Error(nonFieldErrors);
     }
-    throw new Error('Возникла неизвестная ошибка');
+    return response
+}
+
+export async function validateErrorForgotEmail(response: Response): Promise<Response> {
+    if(!response.ok) {
+        const errorObject = await response.json();
+        const nonFieldErrors = errorObject.errors || 'Возможны проблемы с сетью';
+        throw new Error(nonFieldErrors);
+    }
+    return response
 }
